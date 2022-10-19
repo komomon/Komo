ce086aaef8f2f9af6771a240a4e331eba150c406
DEMO\98483 <984839993@qq.com>
2022-10-19 23:16:40 +0800

    naabu 替换为TxPortMap，可以识别端口和指纹，天象后台端口扫描工具

--- core/tools/portscan/portscan_main.py
+++ core/tools/portscan/portscan_main.py
@@ -17,6 +17,7 @@ def create_logfile():
 
 
 def get_system():
+    # global suffix
     platform = sys.platform
     if platform == 'win32':
         suffix = ".exe"
@@ -28,13 +29,15 @@ def get_system():
         exit(1)
 
 
-
+# def manager(domain=None,ip=None,ips=None,ipfile=None,date="2022-09-02-00-01-39"):
 @logger.catch
 def manager(domain=None, ip=None, ipfile=None, date="2022-09-02-00-01-39"):
     suffix = get_system()
     root = os.getcwd()
     pwd_and_file = os.path.abspath(__file__)
-    pwd = os.path.dirname(pwd_and_file)
+    pwd = os.path.dirname(pwd_and_file)  # E:\ccode\python\006_lunzi\core\tools\domain
+
+    # 获取当前目录的前三级目录，即到domain目录下，来寻找exe domain目录下
     grader_father = os.path.abspath(os.path.dirname(pwd_and_file) + os.path.sep + "../..")
     # print(grader_father) # E:\ccode\python\006_lunzi\core
 
@@ -59,6 +62,8 @@ def manager(domain=None, ip=None, ipfile=None, date="2022-09-02-00-01-39"):
         logger.error("[-] Please --domain or --ip or --ipfile")
         exit(1)
 
+    # naabu 可以对域名反查ip然后端口扫描，也可以对ips进行端口扫描
+    # 目前实现对ipfile和子域名的扫描
     @logger.catch
     def naabu(ip=ip, ipfile=ipfile):
         '''
@@ -75,9 +80,33 @@ def manager(domain=None, ip=None, ipfile=None, date="2022-09-02-00-01-39"):
         # print(domain,ip,ips,ipfile)
         outputfile = f'{portscan_log_folder}/{output_filename_prefix}.{sys._getframe().f_code.co_name}.txt'
         cmdstr = f'{pwd}/naabu/naabu{suffix} -source-ip 8.8.8.8:22 -rate 150 -top-ports 100 -silent -no-color -list {ipfile} -o {outputfile}'
+        # naabu -list hosts.txt -p - 扫描全部  -exclude-cdn 跳过cdn检测，cdn只检查80 443
+        # cmd = pwd + f'/naabu{suffix} -p "{ports_str}" -silent -no-color -scan-all-ips -list result/{date}/{domain}.final.subdomains.txt -o {portscan_log_folder}/{domain}.{sys._getframe().f_code.co_name}.txt'
+        # nmap 常见100个端口 -scan-all-ips
+        # cmdstr = f'{pwd}/naabu{suffix} -top-ports 100 -silent -no-color -list result/{date}/{domain}.final.subdomains.txt -o {portscan_log_folder}/{domain}.{sys._getframe().f_code.co_name}.txt'
         logger.info(f"[+] command:{cmdstr}")
         os.system(cmdstr)
-        logger.info(f'[+] naabu finished,outputfile:{outputfile}')
+        logger.info(f'[+] {sys._getframe().f_code.co_name} finished,outputfile:{outputfile}')
+
+    @logger.catch
+    def TxPortMap(ip=ip, ipfile=ipfile):
+        '''
+        TxPortMap 20211210
+        :return:
+        '''
+        logger.info('-' * 10 + f'start {sys._getframe().f_code.co_name}' + '-' * 10)
+        ports_str = "22,80,1433,1521,3389,8009,8080,8443"
+        ports_str = "21,22,23,25,53,53,69,80,81,88,110,111,111,123,123,135,137,139,161,177,389,427,443,445,465,500,515," \
+                    "520,523,548,623,626,636,873,902,1080,1099,1433,1434,1521,1604,1645,1701,1883,1900,2049,2181,2375," \
+                    "2379,2425,3128,3306,3389,4730,5060,5222,5351,5353,5432,5555,5601,5672,5683,5900,5938,5984,6000,6379," \
+                    "7001,7077,8080,8081,8443,8545,8686,9000,9001,9042,9092,9100,9200,9418,9999,11211,11211,27017,33848," \
+                    "37777,50000,50070,61616"
+        outputfile = f'{portscan_log_folder}/{output_filename_prefix}.{sys._getframe().f_code.co_name}.txt'
+        # cmdstr = f'{pwd}/TxPortMap/TxPortMap{suffix} -p {ports_str} -nbtscan -l {ipfile} -o {outputfile}'
+        cmdstr = f'{pwd}/TxPortMap/TxPortMap{suffix} -t1000 -nbtscan -l {ipfile} -o {outputfile}'
+        logger.info(f"[+] command:{cmdstr}")
+        os.system(cmdstr)
+        logger.info(f'[+] {sys._getframe().f_code.co_name} finished,outputfile:{outputfile}')
 
     # win不可用先剔除
     @logger.catch
@@ -94,19 +123,11 @@ def manager(domain=None, ip=None, ipfile=None, date="2022-09-02-00-01-39"):
                     "7001,7077,8080,8081,8443,8545,8686,9000,9001,9042,9092,9100,9200,9418,9999,11211,11211,27017,33848," \
                     "37777,50000,50070,61616"
         # print(domain,ip,ips,ipfile)
-        if domain and ip is None and ipfile is None:
-            file = f'result/{date}/{domain}.subdomains.ips.txt'
-            outputfile = f'{portscan_log_folder}/{domain}.{sys._getframe().f_code.co_name}.txt'
-            cmdstr = f'{pwd}/dismap/dismap{suffix} --file {file} --np -p {ports_str} -o {outputfile}'
-        elif ipfile and domain is None and ip is None:
-            file = ipfile
-            outputfile = f'{portscan_log_folder}/{date}.{sys._getframe().f_code.co_name}.txt'
-            cmdstr = f'{pwd}/dismap/dismap{suffix} --file {file} --np -p {ports_str} -o {outputfile}'
-        else:
-            logger.error("[-] Please --domain or --ips")
-            return False
+        outputfile = f'{portscan_log_folder}/{output_filename_prefix}.{sys._getframe().f_code.co_name}.txt'
+        cmdstr = f'{pwd}/dismap/dismap{suffix} --file {ipfile} --np -p {ports_str} -o {outputfile}'
         logger.info(f"[+] command:{cmdstr}")
         os.system(cmdstr)
+        logger.info(f'[+] {sys._getframe().f_code.co_name} finished,outputfile:{outputfile}')
 
     # 写好了，暂不调用了，这个项目使用了nmap的库，并对端口进行指纹识别，同时也借用了naabu的思路，但是是2020年的，同时使用的是connect连接，不是syn
     @logger.catch
@@ -124,23 +145,17 @@ def manager(domain=None, ip=None, ipfile=None, date="2022-09-02-00-01-39"):
                     "37777,50000,50070,61616"
         # print(domain,ip,ips,ipfile)-host 44.228.249.3 -top-ports -nC -source-ip 8.8.8.8 -o 22.txt
         # -iL 1.txt -top-ports -nC -source-ip 8.8.8.8 -o 22.txt -silent -retries 2
-        if domain and ip is None and ipfile is None:
-            file = f'result/{date}/{domain}.subdomains.ips.txt'
-            outputfile = f'{portscan_log_folder}/{domain}.{sys._getframe().f_code.co_name}.txt'
-            cmdstr = f'{pwd}/nmaps/nmaps{suffix} -top-ports 100 -silent -source-ip 8.8.8.8 -retries 2 -nC -iL {file} -o {outputfile}'
-        elif ipfile and domain is None and ip is None:
-            file = ipfile
-            outputfile = f'{portscan_log_folder}/{date}.{sys._getframe().f_code.co_name}.txt'
-            cmdstr = f'{pwd}/nmaps/nmaps{suffix} -top-ports 100 -silent -source-ip 8.8.8.8 -retries 2 -nC -iL {file} -o {outputfile}'
-        else:
-            logger.error("[-] Please --domain or --ips")
-            exit(1)
+        outputfile = f'{portscan_log_folder}/{output_filename_prefix}.{sys._getframe().f_code.co_name}.txt'
+        cmdstr = f'{pwd}/nmaps/nmaps{suffix} -top-ports 100 -silent -source-ip 8.8.8.8 -retries 2 -nC -iL {ipfile} -o {outputfile}'
         logger.info(f"[+] command:{cmdstr}")
         os.system(cmdstr)
+        logger.info(f'[+] {sys._getframe().f_code.co_name} finished,outputfile:{outputfile}')
 
     def run():
         naabu(ip=None, ipfile=ipfile)
-        # nmaps(ips=None,ipfile=ipfile)
+        TxPortMap(ip=None, ipfile=ipfile)
+        # nmaps(ip=None,ipfile=ipfile)
+        # dismap(ip=None,ipfile=ipfile)
 
     run()
 
@@ -175,3 +190,6 @@ def run(ip=None, ips=None, ipfile=None, date=None):
 
 if __name__ == '__main__':
     fire.Fire(run)
+    # http://testphp.vulnweb.com/vendor
+    # manager(domain="vulnweb.com",ip=None,ips=None,date="2022-09-02-00-01-39")
+    # manager(domain="tiqianle.com", ip=None, date="2022-09-02-00-01-39")
